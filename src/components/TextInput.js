fb34811cbda0e0d2ac0efc3745150f18f7306dda
Ayan Ozturk <a.ozturk@comicrelief.com>
2017-10-19 14:57:47 +0100

    basic components

---
+++ src/components/TextInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TextInput extends React.Component {
+    render() {
+        return (
+            <div>
+                <label>{this.props.label}</label>
+                <input type="text" name={this.props.name} id={this.props.name} />
+            </div>
+        )
+    }
+}
+
+export default TextInput
