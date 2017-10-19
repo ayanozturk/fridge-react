ca9041857995bc638a18ebb047a129e62e9e2f37
Ayan Ozturk <ayanozturk@gmail.com>
2017-10-19 21:35:39 +0100

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import TextInput from './components/TextInput'
-import Button from "./components/Button";
+import TextInput from './components/TextInput';
+import Button from './components/Button';
 
 class App extends React.Component {
-
-    render(){
-        return (
-            <div>
-                <TextInput label='Name' name="name" />
-                <Button>Submit</Button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <TextInput label="Name" name="name" />
+        <Button>Submit</Button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
